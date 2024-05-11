f5344c239a61da629cce1e0cfbbe446100495ece
DeepikaSolanki60 <163406388+DeepikaSolanki60@users.noreply.github.com>
2024-05-11 01:31:22 -0400

    add a card

--- index.php
+++ index.php
@@ -1,50 +1,67 @@
 <html>
-       <head>
-       <title>Restaurant</title>
 
-       <link rel="stylesheet" type="text/css" href="Css/main.css"/> 
-       <link rel="stylesheet" type="text/css" href="Css/navbar.css"/> 
-       <link rel="stylesheet" type="text/css" href="Css/about.css"/> 
-       <script src="javascript/slider.js"></script>
+<head>
+    <title>Restaurant</title>
 
-       </head>
+    <link rel="stylesheet" type="text/css" href="Css/main.css" />
+    <link rel="stylesheet" type="text/css" href="Css/navbar.css" />
+    <link rel="stylesheet" type="text/css" href="Css/about.css" />
+    <link rel="stylesheet" type="text/css" href="Css/card.css"/>
+    <script src="javascript/slider.js"></script>
 
-       
-       <body>
+</head>
 
-       <div class="container" >
-       <div class="row">  
-              
-       <!----------- main area of web site ---------------> 
-        
-       <div class="col12 " >   <!-- container-->   
 
-       <!------------------ Menu Bar ---------------------->
-            
-       <div class="rowmenu">
+<body>
+
+    <div class="container">
+        <div class="row">
+
+            <!----------- main area of web site --------------->
+
+            <div class="col12 "> <!-- container-->
+
+                <!------------------ Menu Bar ---------------------->
+
+                <div class="rowmenu">
+
+                    <?php include "navbar.php" ?>
+
+                </div>
+
+
+                <!------------------- Slider ----------------------->
+
+
+                <div class="rowslider">
+                    <img src="Images/photo1.jpg" width="100%" height="60%" style="margin-top: 5px;" alt="slider1.jpg"
+                        id="slideimage">
+                </div>
+                <br><br>
+                <!-- ---------------------------------------card show---------- -->
+
+                <div class="card"> 
+                    <div class="card_image">
+                      <img src="Images/photo1.jpg"/>
+                    </div>
                 
-                <?php include "navbar.php"?>
-                               
-            </div>  
+                <div class="card_title" >
+                <h1>title Name</h1>
+                </div>
+                <div class="card_des">
+                  <button>read More</button>
+                </div>
+                </div>
+                <!------------------- Footer ----------------------->
 
+                <div class="rowfooter">
 
-             <!------------------- Slider ----------------------->
-            
-                        
-             <div class="rowslider">
-                <img src="Images/photo1.jpg" width="100%"  height="60%"   style="margin-top: 5px;" alt="slider1.jpg" id="slideimage">
-            </div> 
-<br><br>
+                    <?php include "footer.php" ?>
 
-             <!------------------- Footer ----------------------->
-                       
-             <div class="rowfooter">
-                
-                <?php include "footer.php"?>
-                               
-            </div>  
-</div>
-</div>
-</div>
-       </body>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
